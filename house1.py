04cf692466fbf31d0b31bdd35bd232398ac549b0
R_Molokov-wks <rus-molok@yandex.ru>
2021-02-04 11:39:04 +0300

    init

---
+++ house1.py
@@ -0,0 +1,37 @@
+def zakaz():
+    x, y = 400, 500
+    heigh = 400
+    width = 300
+
+    house(x, y, heigh, width)
+
+
+def house_fund(x, y, heigh_fund, width):
+    """
+
+    :param x: точка средина фундамента домика
+    :param y: нижняя точка фундамента домика
+    :param heigh_fund: высота фундамента
+    :param width: ширина фундамента домика
+    :return:
+    """
+    print(f'начальные данные фундамента домика x, y, high_fund, width {x, y, heigh_fund, width}')
+    pass
+
+
+def house(x, y, heigh, width):
+    """
+
+    :param x: точка средина домика
+    :param y: нижняя точка домика
+    :param heigh: высота домика
+    :param width: ширина домика
+    :return:
+    """
+    print(f'начальные данные x,y,height,width {x, y, heigh, width}')
+    height_fund = int(0.05 * heigh)
+    height_wall = int(0.95 * width)
+    house_fund(x, y, height_fund, width)
+
+
+zakaz()
