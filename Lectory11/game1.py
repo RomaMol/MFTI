3e302c5c069fae3fe79d95c50f4549b85f046dfb
R_Molokov-wks <rus-molok@yandex.ru>
2021-02-16 08:25:44 +0300

    add class for game

--- Lectory11/game1.py
+++ Lectory11/game1.py
@@ -1,6 +1,20 @@
 class Player:
+    """Класс игрока, содержит тип значков имя"""
     pass
 
-class GameFields:
+class GameRoundManager:
+    """Менеджер игры, запускает все просесы"""
+    pass
+
+class GameField:
+    """"""
+    pass
+
+class GameFieldView:
+    """"""
+    pass
+
+class GameWindow:
+    """"""
     pass
 
