342111d3118cc56689f425bf5fbdd17db704058a
R_Molokov-wks <rus-molok@yandex.ru>
2021-02-16 08:32:05 +0300

    add class SELL(enum)

--- Lectory11/game1.py
+++ Lectory11/game1.py
@@ -1,20 +1,33 @@
+from enum import Enum
+
+
+class Cell(Enum):
+    """Класс Клетка игрового поля"""
+    VOID = 0  # Пустое поле
+    CROSS = 1  # Кресстик в поле
+    ZERO = 2  # Нолик в поле
+
+
 class Player:
     """Класс игрока, содержит тип значков имя"""
     pass
+
 
 class GameRoundManager:
     """Менеджер игры, запускает все просесы"""
     pass
 
+
 class GameField:
-    """"""
+    """Класс Игровое поле"""
     pass
+
 
 class GameFieldView:
     """"""
     pass
 
-class GameWindow:
-    """"""
-    pass
 
+class GameWindow:
+    """Класс окно игры"""
+    pass
