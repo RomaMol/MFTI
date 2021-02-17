7c404b6ec1ae3bc0901b3c8f8bd7642ba83cfd66
R_Molokov-wks <rus-molok@yandex.ru>
2021-02-17 16:25:44 +0300

    лекция 14 опережающее тестирование

--- Lectory13/test_fib3.py
+++ Lectory13/test_fib3.py
@@ -2,13 +2,11 @@ import fib
 
 final_result = True
 
-test_list = 40
-for n in range(int(test_list)):
-
+test_list = 30
+for n in range(20, int(test_list) + 1, 2):  # началоб конец шаг
     result = fib.fib(n)
     print(f'n {n},  fib.fib(n) {fib.fib(n)}, ')
     n += 1
 
 if final_result:
     print(f"TEST OK")
-
