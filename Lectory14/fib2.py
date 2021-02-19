a8833d064608e850c8c9b4409ce079be140e9f47
R_Molokov-wks <rus-molok@yandex.ru>
2021-02-19 09:46:38 +0300

    лекция 14 тест работает добавлена 2 версия функции

--- Lectory14/fib2.py
+++ Lectory14/fib2.py
@@ -14,12 +14,13 @@ def fib(n):
         raise ValueError("fin fun can not work if n > 1000")
     if n == 0:
         return 0
-    
-    f = [0, 1] + [0] * (n - 1)
-    for i in range(2, n + 1):
-        f[i] = f[i - 1] + f[i - 2]
 
-    return f[n]
+    f_2 = 0
+    f_1 = 1
+    for i in range(2, n + 1):
+        f_1, f_2 = (f_1+f_2), f_1
+
+    return f_1
 
 
 if __name__ == "__main__":
--- Lectory14/main.py
+++ Lectory14/main.py
@@ -1,5 +1,5 @@
 # Опережающее тестирование
-from Lectory14 import fib
+from Lectory14 import fib2, fib
 
 
 def main():
@@ -7,6 +7,7 @@ def main():
     """Запуск программы"""
     n = int(input("Введите номер числа Фибоначи:  "))
     print(f'Ваше число Фибоначи:  {fib.fib(n)}')
+    print(f'Ваше число Фибоначи:  {fib2.fib(n)}')
 
 if __name__ == '__main__':
     main()
