5db72f09d47fbcf4f55fdf5765fb8aa81ce79dea
R_Molokov-wks <rus-molok@yandex.ru>
2021-03-18 12:54:40 +0300

    lec6 while

---
+++ Lec6/main-for.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=YY5cICJTDEU
+#   Python 3 #6: операторы циклов while и for, операторы break и continue
+
+# for <переменная> in <список>:
+#    <оператор>
