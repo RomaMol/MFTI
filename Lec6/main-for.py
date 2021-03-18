cdca8bb30b7ea1100e69f61687e73fc702dd8b8e
Admin <rus-molok@yandex.ru>
2021-03-18 22:30:50 +0300

    python lec6 while  for in

--- Lec6/main-for.py
+++ Lec6/main-for.py
@@ -6,3 +6,33 @@
 
 # for <переменная> in <список>:
 #    <оператор>
+
+for x in 1, 5, 2, 4:
+    print(x ** 2)
+
+# Для этого используется генератор последовательностей
+# range(start, stop, step)
+# прямая последовательность
+for x in range(1, 5, 1):
+    print("прямая последовательность  ", x)
+
+# обратная последовательность
+for x in range(5, 0, -1):
+    print("обратная последовательность  ", x)
+
+S=0
+for i in range(1, 1001, 1):
+    S += 1/i
+print(S)
+
+
+k = 0.5
+b = 2
+lst = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+for x in lst:
+    print(f"k {k }, b {b}, x {x}, lst{lst}")
+    print("x*k+b", x*k+b)
+
+msg = "Hello World!"
+for x in msg:
+    print(x)
