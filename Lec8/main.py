7fadc184bd4486e09376d954ecbedb86e68e14bc
R_Molokov-wks <rus-molok@yandex.ru>
2021-03-23 09:00:56 +0300

    lec8

---
+++ Lec8/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=4LDjbKQS9PA
+#   Python 3 #8: методы строк - upper, split, join, find, strip, isalpha, isdigit и другие
+
+
