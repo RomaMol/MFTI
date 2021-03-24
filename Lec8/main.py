249dd086aff7c44bf5e1affd11999e7e7caeeabf
R_Molokov-wks <rus-molok@yandex.ru>
2021-03-24 08:32:27 +0300

    lec8

--- Lec8/main.py
+++ Lec8/main.py
@@ -5,3 +5,20 @@
 #   Python 3 #8: методы строк - upper, split, join, find, strip, isalpha, isdigit и другие
 
 
+#  строка.имя_метода(аргументы)
+
+string_user = "abrakadabra"
+print(f'string_user.upper() = {string_user.upper()}')
+print(f'string_user.lower() = {string_user.lower()}')
+
+print(f'string_user.count("ra") = {string_user.count("ra")}')
+
+# String.find(sub[, start[, end]])  count("ra", 4, 10)
+print(f'string_user.count("ra", 4, 10) = {string_user.count("ra", 4, 10)}')
+
+print(f'string_user.replace("ab", "AB") = {string_user.replace("ab", "AB")}')
+print(f'string_user.replace("ab", "") = {string_user.replace("ab", "")}')
+
+# Третий необязательный аргумент задает максимальное количество замен. Например:
+
+print(f'string_user.replace("ab", "AB") = {string_user.replace("ab", "AB",1)}')
