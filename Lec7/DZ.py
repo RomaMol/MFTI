35e2f1d86a5f30979003d61593c6a458b3023708
R_Molokov-wks <rus-molok@yandex.ru>
2021-03-26 08:49:24 +0300

    lec7 dz

--- Lec7/DZ.py
+++ Lec7/DZ.py
@@ -14,33 +14,31 @@ print(f'подсчет буквы "а" количество = {kol_vo}')
 
 #  удаление из строки "аb"
 bad_chars = 'ab'
-
-new_text = ''
 number = 0
-print("len(text)", len(text))
+text1 = ''
 while number < len(text):
     print(number)
-    chars1 = text[number]
     if number + 1 >= len(text):
+        chars1 = text[number]
+        text1 = text1 + chars1
+        print("chars1", chars1, "text1", text1)
         break
     else:
+        chars1 = text[number]
         chars2 = text[number + 1]
-        print("chars1 == bad_chars1[0]", chars1)
-        print("chars2 == bad_chars1[1]", chars2)
         test = chars1 + chars2
-        print("test = ", test, 'bad_chars = ', bad_chars)
+        print("chars1", chars1, "chars2", chars2)
+        print("test", test, "bad_chars", bad_chars)
         if test == bad_chars:
-            number += 1
             chars1 = ''
             chars2 = ''
-    if number <= len(text):
+            number += 1
+            # else:
         number += 1
-        new_text = new_text + chars1
-    else:
-        new_text = new_text + chars2
+        text1 = text1 + chars1
+        print('text1', text1)
 
-    print('chars', chars1, chars2, new_text)
+print(' while number < len(text):  text1', text1)
 
 text2 = text.replace("ab", "")
-
 print(f'удаление из строки "{text}"  "аb"  = {text2}')
