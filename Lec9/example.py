3a0884224d6bfddd5a7ebfb23c4629f59c096bec
Admin <rus-molok@yandex.ru>
2021-03-27 22:05:02 +0300

    python lec9 example

--- Lec9/example.py
+++ Lec9/example.py
@@ -6,9 +6,9 @@
 
 
 lst = [-1, 0, 5, 3, 2]
-
-for x in lst:
-    print(f'x = {x},   x*7.2 = {x * 7.2} ')
+print(f'lst {lst}')
+for x in range(5):
+    print(f'x = {x}, lst[x] = {lst[x]},   lst[x]*7.2 = {lst[x] * 7.2} ')
 print(lst)
 print()
 
@@ -16,3 +16,11 @@ for x in range(len(lst)):
     lst[x] = lst[x] * 7.2
     print(f'x = {x}, lst[x]*7.2 = {lst[x]}')
 print(lst)
+print()
+nlst = []
+for x in lst:
+    x = int(x / 7.2)
+    print(f'x = {x}')
+    nlst.append(x)
+print(f'lst {lst}')
+print(f'nlst {nlst}')
