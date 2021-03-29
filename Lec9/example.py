191235127da1958c491048ace036bd9a559c6b90
R_Molokov-wks <rus-molok@yandex.ru>
2021-03-29 10:11:43 +0300

    lec10

--- Lec9/example.py
+++ Lec9/example.py
@@ -31,15 +31,15 @@ print(f'nlst {nlst}')
 
 m1 = [[1, 2], [3, 4], [5, 6]]
 m2 = [[1, 0], [0, 1], [1, 1]]
-m3 = m1
+#m3 = m1
 print(f'm1 + m2  = {m1},+ {m2}')
 
-for i in range(len(m3)):
+for i in range(len(m1)):
     print(f'i =  {i}')
-    m3[i][0] = m1[i][0] + m2[i][0]
-    m3[i][1] = m1[i][1] + m2[i][1]
+    m1[i][0] = m1[i][0] + m2[i][0]
+    m1[i][1] = m1[i][1] + m2[i][1]
 
-print("summ matr",m3)
+print("summ matr m1 ", m1)
 
 x = [1, 2, 3, 4, 5, 6]
 y = [1, 0, 0, 1, 1, 1]
---
+++ lec10/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=v8Q6nR7itsU
+#   Python 3 #10: списки - срезы и методы: append, insert, pop, sort, index, count, reverse, clear
+
+
+
