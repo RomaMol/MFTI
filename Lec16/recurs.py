8a64c3d5da3e833d00f5206e355b2d73768e7b40
Admin <rus-molok@yandex.ru>
2021-03-31 22:22:44 +0300

    python lec16

---
+++ Lec16/recurs.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=z6b_F3OC39s
+#   Python 3 #16: рекурсивные и лямбда-функции, функции с произвольным числом аргументов
