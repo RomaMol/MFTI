45b9ad134a055d3b6ea2f5fd56b9c36e3e3de07f
R_Molokov-wks <rus-molok@yandex.ru>
2021-04-06 09:05:01 +0300

    lec16

--- Lec16/showelement.py
+++ Lec16/showelement.py
@@ -4,9 +4,16 @@ def showelement(lst, fun):
             print(x)
 
 
+lst = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-lst =(1,2,3,4,5,6,7,8,9)
+
 def odd(x):
-    return True if x%2!=0 else False
+    return True if x % 2 != 0 else False
+
 
 showelement(lst, odd)
+
+lambda_funk = lambda a, b: a + b
+print(lambda_funk(10, 15))
+
+showelement(lst, lambda x: True if x % 2 == 0 else False)
---
+++ Lec17/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=8HvcVQWR2gY
+#   Python 3 #17: алгоритм Евклида, принцип тестирования программ
