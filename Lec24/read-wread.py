a0a7f0f22dd9f95b3e316bddbd51bf68f09a8231
Admin <rus-molok@yandex.ru>
2021-06-10 23:53:39 +0300

    python lec24 open file

--- Lec24/read-wread.py
+++ Lec24/read-wread.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#   https://www.youtube.com/watch?v=Ccry8wMJ39o
-#   Python 3 #23: обработка исключений: try, except, finally, else
+#   https://www.youtube.com/watch?v=bnxlN1veGzk
+#   Python 3 #24: файлы - чтение и запись: open, read, write, seek, readline, dump, load, pickle
+# 'file' = Путь к файлу вместе с именем
+#  mode="r" режим чтения иди "w" запись файла
+#  encoding=None  кодировка файла
+# "test.txt"  --> D:\2020-PYTHON\MFTI\Lec24\test.txt
+
+# D:\\2020-PYTHON\\MFTI\\Lec24\\test.txt -->  D:/2020-PYTHON/MFTI/Lec24/test.txt
+# D:/2020-PYTHON/outtest.txt  --> ../outtest.txt
+# D:/2020-PYTHON/2021Corpnet/intest.txt -->  ../2021Corpnet/intest.txt
+
+
+# open('file',mode="r", encoding=None)
+try:
+    file1 = open('test.txt')
+except FileNotFoundError:
+    print("Файл ненайден")
+
