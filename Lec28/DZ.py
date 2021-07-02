b50dd3cca960e8d4178af817fc154c6c20d939a3
Администратор <rus-molok@yandex.ru>
2021-07-02 15:26:19 +0300

    lec 28 DZ

--- Lec28/DZ.py
+++ Lec28/DZ.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+#   https://www.youtube.com/watch?v=TdeZKWBppo4
+#   Python 3 #28: декораторы функций и замыкания
+
 import time
 
 
 def testtime(fn):
-    """Функция декаратор
+    """Функция декоратор
     Функция проверки времени выполнения функции getnod """
 
     def wraper(*args, **kwargs):
