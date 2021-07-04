71af6d91e6911b4fbfa9592d9247b7299fa15e52
Admin <rus-molok@yandex.ru>
2021-07-04 22:51:22 +0300

    python lec29

--- Lec29/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-#   https://www.youtube.com/watch?v=-_qo6YIWheU
-#   Python 3 #29: установка и порядок работы в PyCharm
-#  https://www.jetbrains.com/pycharm/
-
