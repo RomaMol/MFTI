577fa108b0fb0158886b27b3adc4e7968b38de64
Admin <rus-molok@yandex.ru>
2021-07-04 23:25:10 +0300

    python lec 30 and DZ

--- Lec30/DZ.py
+++ Lec30/DZ.py
@@ -5,17 +5,27 @@
 #   Python 3 #30: функция enumerate, примеры использования
 
 A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(f"до применения функция enumerate {A}")
+try:
+    n = int(input(f"Введите число от 0 до 2 :"))
+except:
+    print("Неправильное число, Введите число от 0 до 2 : ")
 
-for element in enumerate(A):  # функция enumerate(spisok, start = 0) начальное значение индекса
-    # print(f'картеж {element}')
-    for i, g in enumerate(element):
-        # print(f'element2 {g}')
-        if isinstance(g, list):
-            for i, g in enumerate(g):
-                print(f'element2 {i, g}')
-                if i == 0:
-                    g = 0
-                    print(g)
-                i += 1
 
-print(A)
+def modelist():
+    """изменение листа по заявленным правилам"""
+    for element in enumerate(A):  # функция enumerate(spisok, start = 0) начальное значение индекса
+        # print(f'картеж {element}')
+        for i, g in enumerate(element):
+            # print(f'element2 {g}')
+            if isinstance(g, list):
+                for i, x in enumerate(g):
+                    # print(f'element2 {i, x}')
+                    if i == n:  # порядковый элемент списка   [1, 2, 0]
+                        g[i] = 0  # изменяем соответствующее значение  с 3 на 0 [1, 2, 0]
+                        # print("g = ", g)
+                    i += 1
+
+
+modelist()
+print(f" измененный список применения функция enumerate {A}")
