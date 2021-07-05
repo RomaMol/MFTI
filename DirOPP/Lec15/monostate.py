20535128a056233733cdf7e5b8fa61dceea258ac
Администратор <rus-molok@yandex.ru>
2021-07-05 16:43:43 +0300

    lec15 monostate

--- DirOPP/Lec15/monostate.py
+++ DirOPP/Lec15/monostate.py
@@ -6,3 +6,30 @@
 #    #15. ООП Python 3: Моносостояние экземпляров класса
 
 
+# пример реализации Моносостояния
+
+class Trenddata:
+    """класс для некого числа одинаковых потоков"""
+    __common_attrs = {"name": "trend_1",
+                      "data": {},
+                      "id": 1
+                      }
+
+    def __init__(self):
+        self.__dict__ = Trenddata.__common_attrs
+
+
+td1 = Trenddata()
+td2 = Trenddata()
+print(f"td1.id = {td1.id}, td2.id = {td2.id}")
+td1.id = 3
+print(f"td1.id = 3 ")
+
+print(f"td1.id = {td1.id}, td2.id = {td2.id}")
+
+td2.new_attr = "new attr"
+print(f"    td2.new_attr =  ")
+print(f"td1.__dict__ = {td1.__dict__}")
+print(f"td2.__dict__ = {td2.__dict__}")
+
+print(f"id td1 = {id(td1.__dict__)}, id td2 = {id(td2.__dict__)}")
