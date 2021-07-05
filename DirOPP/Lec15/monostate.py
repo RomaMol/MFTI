7831d90af52d09f2c01d21512088ab37c290fb5b
Admin <rus-molok@yandex.ru>
2021-07-05 23:57:44 +0300

    python OPP lec 17

--- DirOPP/Lec15/monostate.py
+++ DirOPP/Lec15/monostate.py
@@ -3,7 +3,7 @@
 
 #    https://www.youtube.com/watch?v=voRJv-t2SiA&list=PLA0M1Bcd0w8zo9ND-7yEFjoHBg_fzaQ-B&index=15
 
-#    #15. ООП Python 3: Моносостояние экземпляров класса
+#    15. ООП Python 3: Моносостояние экземпляров класса
 
 
 # пример реализации Моносостояния
