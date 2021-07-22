926fcaff572074a1734065a2fc24370906f460f2
Администратор <rus-molok@yandex.ru>
2021-07-22 16:49:17 +0300

    lec7 sizer S lec8

--- CorpNetLocal/CorpNetExe.py
+++ CorpNetLocal/CorpNetExe.py
@@ -9,6 +9,7 @@ from CorpNetLocal.menu.progmenu import MyFrame
 if __name__ == '__main__':
     CorpNet = wx.App()
     window = MyFrame(None, title="CorpNet-Local", )
+    the_first_run()
     window.Center()
     window.Show()
     CorpNet.MainLoop()
--- CorpNetLocal/menu/configtxt.py
+++ CorpNetLocal/menu/configtxt.py
@@ -4,18 +4,17 @@
 import os
 
 one_ini = "\n\n\nN, №, N п.п, №п.п, № п.п, №п/п, № п/п\n\n\n"
-too_ini = "name1\nlogin1\nip_address1\npassword1\n" \
-          "name2\nlogin2\nip_address2\npassword2\n"
-conf_file_ini = ['config.ini', 'config2.ini']
-ini = [one_ini, too_ini]
+
+conf_file_ini = ['config.ini']
+ini = [one_ini]
 
 
 def the_first_run():
     """ первый запуск программы проверка файла конфиг если нет создание"""
 
     one = os.path.exists(conf_file_ini[0])
-    too = os.path.exists(conf_file_ini[1])
-    if one or too is True:
+
+    if one  is True:
         return
 
     for file in conf_file_ini:
